b9e32988217d71c540e88ba79d698060bb1b741a
Scott Gaines <scottdgaines@gmail.com>
2023-09-22 13:59:26 -0600

    Add files and directories for basic architecture

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Sidebar from '../Sidebar/Sidebar'
+import Grid from '../Grid/Grid'
+
+function App() {
+  return (
+    <div>
+      <Sidebar />
+      <Grid />
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div>
+      <p>sidebar</p>
+    </div>
+  )
+}
+
+export default SideBar
