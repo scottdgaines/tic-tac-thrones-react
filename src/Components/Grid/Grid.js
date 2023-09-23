0e9a1c9bfd60b6d016519f2e7aab7b10586f4778
Scott Gaines <scottdgaines@gmail.com>
2023-09-23 10:22:35 -0600

    Add HTML structure to grid

--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import './Grid.css'
 
 const Grid = () => {
   return (
-    <div>
-      <p></p>
-    </div>
+    <>
+        <p>Tic-Tac-Thrones</p>
+        <div className="grid"> 
+            <article className="tile t1" id={1}></article>
+            <article className="tile t1" id={2}></article>
+            <article className="tile t1" id={3}></article>
+            <article className="tile t1" id={4}></article>
+            <article className="tile t1" id={5}></article>
+            <article className="tile t1" id={6}></article>
+            <article className="tile t1" id={7}></article>
+            <article className="tile t1" id={8}></article>
+            <article className="tile t1" id={9}></article>
+        </div>
+    
+    </>
   )
 }
 
